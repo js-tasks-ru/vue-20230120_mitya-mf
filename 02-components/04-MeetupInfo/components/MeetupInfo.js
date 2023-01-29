ff4d6024f3171421cafc0d5f9e8386feac27a8d4
Dmitrii Kaplunov <mail@ae-studio.ru>
2023-01-29 03:01:48 +0300

    Задача 02/04 MeetupInfo готова

--- 02-components/04-MeetupInfo/components/MeetupInfo.js
+++ 02-components/04-MeetupInfo/components/MeetupInfo.js
@@ -2,20 +2,40 @@ import { defineComponent } from '../vendor/vue.esm-browser.js';
 
 export default defineComponent({
   name: 'MeetupInfo',
+  props: {
+    organizer: {
+      required: true,
+      type: String
+    },
+    place: {
+      required: true,
+      type: String
+    },
+    date: {
+      required: true,
+      type: Number
+    }
+  },
+  computed: {
+    formattedDate() {
+      const d = new Date(this.date);
+      return d.toLocaleDateString(navigator.language, {day: 'numeric', month: 'long', year: 'numeric'})
+    }
+  },
 
   template: `
     <ul class="meetup-info">
       <li>
         <img class="icon meetup-info__icon" alt="icon" src="/assets/icons/icon-user.svg" />
-        Организатор
+        {{ organizer }}
       </li>
       <li>
         <img class="icon meetup-info__icon" alt="icon" src="/assets/icons/icon-map.svg" />
-        Место
+        {{ place }}
       </li>
       <li>
         <img class="icon meetup-info__icon" alt="icon" src="/assets/icons/icon-cal-lg.svg" />
-        <time datetime="2020-01-01">1 янв. 2020</time>
+        <time datetime="2020-01-01">{{ formattedDate }}</time>
       </li>
     </ul>`,
 });
