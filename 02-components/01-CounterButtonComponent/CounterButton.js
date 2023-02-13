f183889749be44b6980f60a32a02e2572ec49522
Dmitrii Kaplunov <mail@ae-studio.ru>
2023-02-13 18:47:35 +0300

    1st way

--- 02-components/01-CounterButtonComponent/CounterButton.js
+++ 02-components/01-CounterButtonComponent/CounterButton.js
@@ -5,15 +5,25 @@ export default defineComponent({
 
   props: {
     count: {
-      default: 0
+      default: 0,
+      type: Number
+    },
+ 
+  },
+  emits: ['setIncreasedCount'],
+
+  methods: {
+    // increment(value) {
+    //   this.$emit('countIncrement', value++)
+    // }
+    increaseCount(value) {
+      this.$emit('setIncreasedCount', ++value)
     }
   },
-  emits: ['update:count'],
+
 
   // Компонент должен иметь входной параметр и порождать событие
 
-  template: `<button type="button"
-          :value="count"
-          @click="$emit('update:count', ++$event.target.value)">
+  template: `<button type="button" @click="increaseCount(count)">
             {{ count }}</button>`,
 });
