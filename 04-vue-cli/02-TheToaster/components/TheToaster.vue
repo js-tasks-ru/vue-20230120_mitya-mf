2a6366f640f10f3c34cb67d0d98b75e689a2e2c0
Dmitrii Kaplunov <mail@ae-studio.ru>
2023-02-15 03:02:48 +0300

    Частично готово, не корректно работает удаление по таймауту

--- 04-vue-cli/02-TheToaster/components/TheToaster.vue
+++ 04-vue-cli/02-TheToaster/components/TheToaster.vue
@@ -1,24 +1,48 @@
 <template>
   <div class="toasts">
-    <div class="toast toast_success">
-      <UiIcon class="toast__icon" icon="check-circle" />
-      <span>Success Toast Example</span>
-    </div>
+    <UiToast v-for="toast in toasts" :toast="toast" @removeEl="remove()" :data-id="toast.id"/>
 
-    <div class="toast toast_error">
-      <UiIcon class="toast__icon" icon="alert-circle" />
-      <span>Error Toast Example</span>
-    </div>
   </div>
 </template>
 
 <script>
-import UiIcon from './UiIcon.vue';
+
+import UiToast from './UiToast.vue';
 
 export default {
   name: 'TheToaster',
+  data() {
+    return {
+      toasts: [],
+      id: 0
+    }
+  },
+  methods: {
+    success(msg) {
+      this.toasts.push({
+        message: msg,
+        statusClass: 'toast_success',
+        icon: 'check-circle',
+        id: ++this.id
+      })
+      
+    },
+    error(msg) {
+      this.toasts.push({
+        message: msg,
+        statusClass: 'toast_error',
+        icon: 'alert-circle',
+        id: ++this.id
+      })
+    
+    },
+    remove() {
+      this.toasts.shift()
+      console.log('Удаляем')
+    }
+  },
 
-  components: { UiIcon },
+  components: { UiToast },
 };
 </script>
 
@@ -41,33 +65,5 @@ export default {
   }
 }
 
-.toast {
-  display: flex;
-  flex: 0 0 auto;
-  flex-direction: row;
-  align-items: center;
-  padding: 16px;
-  background: #ffffff;
-  box-shadow: 0 2px 6px rgba(0, 0, 0, 0.15);
-  border-radius: 4px;
-  font-size: 18px;
-  line-height: 28px;
-  width: auto;
-}
 
-.toast + .toast {
-  margin-top: 20px;
-}
-
-.toast__icon {
-  margin-right: 12px;
-}
-
-.toast.toast_success {
-  color: var(--green);
-}
-
-.toast.toast_error {
-  color: var(--red);
-}
 </style>
